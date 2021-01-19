3d927bc6dd56e28e35525cf7e35de21f43a2b51b
Weslley Fratini <weeslleey354@gmail.com>
2021-01-19 20:42:00 -0300

    ambiente

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+require("dotenv").config;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const { HTTP_PORT } = process.env;
+
+app.listen(HTTP_PORT, () => {
+  console.log(`Estou conectado na porta ${HTTP_PORT}`);
+});
