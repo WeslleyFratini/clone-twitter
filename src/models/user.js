a2f57559e57ce0146ee4c473c773d0abb1f0241c
Weslley Fratini <weeslleey354@gmail.com>
2021-02-01 20:28:10 -0300

    criação do model

---
+++ src/models/user.js
@@ -0,0 +1,18 @@
+const { ObjectId } = require("mongodb");
+const mongooose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const User = new Schema({
+  name: { type: String, required: true },
+  age: { type: String, required: true },
+  bio: { type: String, required: true },
+  user: { type: String, required: true },
+  location: { type: String, required: true },
+  posts: [{ type: ObjectId, ref: "Post" }],
+  email: { type: String, required: true },
+  profile_pic: { type: String, required: true },
+  birth_date: { type: Date, required: true },
+});
+
+module.exports = mongoose.model("user", "user");
