cf135891576ef84894267dcd9187d48cff6382cc
Weslley Fratini <weeslleey354@gmail.com>
2021-02-03 09:38:05 -0300

    autenticação jwt

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,11 +1,29 @@
 const jwt = require("jsonwebtoken");
 
-const AuthMiddleware = (req, res) => {
-  const { authorization } = req.headers;
-  const { JWT_SECRET } = process.env;
+const excludeUrls = ["/login"];
 
-  const decoded = jwt.verify(authorization, JWT_SECRET);
-  next();
+const AuthMiddleware = (req, res, next) => {
+  if (excludeUrls.includes(req.path)) {
+    next();
+  } else {
+    const { authorization } = req.headers;
+    const { JWT_SECRET } = process.env;
+
+    if (!authorization) {
+      res.send({ message: "Url requer autenticação" });
+    }
+
+    const decoded = jwt.verify(authorization, JWT_SECRET);
+    const now = moment().unix();
+
+    if (now > decoded.exp) {
+      res.send({ message: "token expirado" });
+    }
+
+    req.decoded = decoded;
+
+    next();
+  }
 };
 
 module.exports = AuthMiddleware;
