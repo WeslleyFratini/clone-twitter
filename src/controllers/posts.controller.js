fdd0d9bdc32213494d0ed936502f8a41047a1457
Weslley Fratini <weeslleey354@gmail.com>
2021-02-03 09:58:21 -0300

    services

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,25 +1,14 @@
-const PostsModel = require("../models/Post");
-const UserModel = require("../models/User");
+const PostsService = require("../services/posts.service");
 
 module.exports = {
   async create(req, res) {
     try {
       const { body } = req;
       const { user } = body;
-      if (!body.content) {
-        throw new Error("O content não existe");
-      }
 
-      const postInstance = new PostModel(body);
+      const response = await PostsService.create(body, user);
 
-      const response = await postInstance.save();
-
-      const postedByUser = await UserModel.findOneAndUpdate(
-        { user },
-        { $push: { posts: response._id } }
-      );
-
-      res.send({ ...response, user: postedByUser });
+      res.send(response);
     } catch (error) {
       res.status(500).send({ error: true });
     }
---
+++ src/services/posts.service.js
@@ -0,0 +1,21 @@
+const PostsModel = require("../models/Post");
+const UserModel = require("../models/User");
+
+module.exports = {
+  async create(data, user) {
+    if (!body.content) {
+      throw new Error("O content não existe");
+    }
+
+    const postInstance = new PostModel(data);
+
+    const response = await postInstance.save();
+
+    const postedByUser = await UserModel.findOneAndUpdate(
+      { user },
+      { $push: { posts: response._id } }
+    );
+
+    return { ...response, user: postedByUser };
+  },
+};
